56e07ee7899875b46e63468c5482a3cafe7aef2d
henryye20 <henryye2006@gmail.com>
2023-04-11 08:34:52 -0700

    added debuglog messages to the remove and add stats

--- TristanAutoBattler/Assets/Scripts/Character/Self.cs
+++ TristanAutoBattler/Assets/Scripts/Character/Self.cs
@@ -20,46 +20,60 @@ public class Self : MonoBehaviour
     
     public void addL(int p){
         level += p;
+        Debug.Log("added "+p+" level(s)");
     }
     public void addH(int p){
         maxhealth += p;
+        Debug.Log("added "+p+" health");
     }
     public void addA(int p){
         atk += p;
+        Debug.Log("added "+p+" attack");
     }
     public void addAS(int p){
         atkspeed += p;
+        Debug.Log("added "+p+" attackspeed");
     }
     public void addD(int p){
         defense += p;
+        Debug.Log("added "+p+" defense");
     }
     public void addCL(int p){
         critluck += p;
+        Debug.Log("added "+p+" critluck");
     }
     public void addIL(int p){
         itemluck += p;
+        Debug.Log("added "+p+" itemluck");
     }
 
     public void removeL(int p){
         level -= p;
+        Debug.Log("removed "+p+" level(s)");
     }
     public void removeH(int p){
         maxhealth -= p;
+        Debug.Log("removed "+p+" health");
     }
     public void removeA(int p){
         atk -= p;
+        Debug.Log("removed "+p+" attack");
     }
     public void removeAS(int p){
         atkspeed -= p;
+        Debug.Log("removed "+p+" attackspeed");
     }
     public void removeD(int p){
         defense -= p;
+        Debug.Log("removed "+p+" defense");
     }
     public void removeCL(int p){
         critluck -= p;
+        Debug.Log("removed "+p+" critluck");
     }
     public void removeIL(int p){
         itemluck -= p;
+        Debug.Log("removed "+p+" itemluck");
     }
 
     // Update is called once per frame
