65575a74205e90c8b8382de0f5eff57bfe47dcd4
henryye20 <henryye2006@gmail.com>
2023-05-30 09:43:16 -0700

    Fix

--- TristanAutoBattler/Assets/Scripts/ChangeScene.cs
+++ TristanAutoBattler/Assets/Scripts/ChangeScene.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
 {
+    //pootis ootis otis tis is s is tis otis ootis pootis
+
     public void MoveToScene(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
